871a0520987b4d11a37b7870f9216cc8c6cd197e
Jorgenna Benitez <jorgennabenitez@Jorgennas-MacBook-Pro.local>
2017-05-07 22:08:54 -0700

    first commit

---
+++ src/com/jorgennabenitez/Main.java
@@ -0,0 +1,18 @@
+package com.jorgennabenitez;
+
+public class Main {
+
+    public static void main(String[] args) {
+    BankAccount jorgie = new BankAccount();
+//    jorgie.setAccountNumber(11);
+//    jorgie.setBalance(100.00);
+//    jorgie.setCustomerName("Jorgenna Benitez");
+//    jorgie.setEmail("jorgie@mush.com");
+//    jorgie.setPhoneNumber("(123)-456-7890");
+
+        System.out.println(jorgie.getAccountNumber());
+        System.out.println(jorgie.getBalance());
+        jorgie.withdrawl(10.00);
+        jorgie.deposit(20.00);
+    }
+}
